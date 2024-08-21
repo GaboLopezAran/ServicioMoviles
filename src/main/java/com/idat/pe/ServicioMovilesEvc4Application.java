ae57a05dc1092bf97a714a02ddaac9269c93fc6e
GaboLópezAran <150459446+GaboLopezAran@users.noreply.github.com>
2024-08-20 22:59:36 -0500

    Primer commit de servicio moviles

---
+++ src/main/java/com/idat/pe/ServicioMovilesEvc4Application.java
@@ -0,0 +1,13 @@
+package com.idat.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioMovilesEvc4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioMovilesEvc4Application.class, args);
+	}
+
+}
